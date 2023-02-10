f4fce85e718d77cf20349899e758bf654d5472ed
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-02-10 10:55:29 +0200

    recipeSearch works

--- App.js
+++ App.js
@@ -1,10 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Alert,Button, Image,TextInput } from 'react-native';
 
 export default function App() {
+
+  const [ingredient, setIngredient] = useState('')
+  const [recipies, setRecipies] = useState([]);
+
+  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
+
+
+
+  const getRecipes = () => {
+    fetch(url)
+    .then(response => response.json())
+    .then(responseJson => setRecipies(responseJson.meals))
+    
+    .catch(error => { 
+        Alert.alert('Error', error); 
+    });    
+  }
+  
+
+  const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "80%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.searchView}>
+        <TextInput 
+      style={{fontSize: 18, width: 200, border: 2}} 
+      placeholder='ingredient' 
+      value={ingredient}
+      onChangeText={text => setIngredient(text)} 
+     />
+    <Button title="Find" onPress={getRecipes} />
+
+    </View>
+
+      <FlatList 
+        style={{marginLeft : "5%"}}
+        keyExtractor={(item, index) => index.toString()} 
+        renderItem={({item}) => 
+          <View>
+            <Text style={{fontSize: 18, fontWeight: "bold"}}>{item.strMeal}</Text>
+            <Image style={styles.tinyLogo} source={{uri: item.strMealThumb}} ></Image>
+          </View>}
+        data={recipies} 
+        ItemSeparatorComponent={listSeparator} /> 
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +70,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 10
   },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  searchView: {
+    marginTop:30,
+    borderWidth: 4
+  }
 });
